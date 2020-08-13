8b953e2c8ac8b6339da344aa1c5f560487917459
hoangdanghung <hunghd@funtap.vn>
2020-08-13 11:28:47 +0700

    first commit

---
+++ config/services.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+        'webhook' => [
+            'secret' => env('STRIPE_WEBHOOK_SECRET'),
+            'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
+        ],
+    ],
+    'facebook' => [
+        'page-token' => env('FACEBOOK_PAGE_TOKEN', '100739581750943'),
+
+            // Optional - Omit this if you want to use default version.
+        'version'    => env('FACEBOOK_GRAPH_API_VERSION', '4.0'),
+
+        // Optional - If set, the appsecret_proof will be sent to verify your page-token.
+        'app-secret' => env('FACEBOOK_APP_SECRET', '04a69088e159e30fbc6198a2bc2b6781')
+    ],
+
+];
